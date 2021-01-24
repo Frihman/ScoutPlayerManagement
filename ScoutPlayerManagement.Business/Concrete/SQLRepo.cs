6734634f040c8431b2df7324def33b21660cb489
James Frih <jamesfrih@me.com>
2021-01-24 23:23:40 +0100

    created IRepository and a class using it

---
+++ ScoutPlayerManagement.Business/Concrete/SQLRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoutPlayerManagement.Business.Concrete
+{
+    public class SQLRepo : IRepository
+    {
+        private ScoutDBEntities _data;
+        public SQLRepo()
+        {
+            _data = new ScoutDBEntities();
+        }
+        public void Add(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return _data.Players.ToList();
+        }
+    }
+}
