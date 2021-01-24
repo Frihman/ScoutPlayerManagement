0ed74ae894cbbcf8128958580f29f06c0c603ef1
James Frih <jamesfrih@me.com>
2021-01-24 17:00:02 +0100

    designed the form (corrected)

--- ScoutPlayerManagement.Windows.UI/Form1.Designer.cs
+++ ScoutPlayerManagement.Windows.UI/Form1.Designer.cs
@@ -28,13 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblNationality = new System.Windows.Forms.Label();
+            this.lblSuitability = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(354, 290);
+            this.listBox1.TabIndex = 0;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 316);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Location = new System.Drawing.Point(69, 316);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(44, 13);
+            this.lblPosition.TabIndex = 2;
+            this.lblPosition.Text = "Position";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(12, 344);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 3;
+            this.lblAge.Text = "Age";
+            // 
+            // lblNationality
+            // 
+            this.lblNationality.AutoSize = true;
+            this.lblNationality.Location = new System.Drawing.Point(12, 368);
+            this.lblNationality.Name = "lblNationality";
+            this.lblNationality.Size = new System.Drawing.Size(56, 13);
+            this.lblNationality.TabIndex = 4;
+            this.lblNationality.Text = "Nationality";
+            // 
+            // lblSuitability
+            // 
+            this.lblSuitability.AutoSize = true;
+            this.lblSuitability.Location = new System.Drawing.Point(12, 392);
+            this.lblSuitability.Name = "lblSuitability";
+            this.lblSuitability.Size = new System.Drawing.Size(51, 13);
+            this.lblSuitability.TabIndex = 5;
+            this.lblSuitability.Text = "Suitability";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.Location = new System.Drawing.Point(157, 316);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(209, 89);
+            this.lblDescription.TabIndex = 6;
+            this.lblDescription.Text = "Description";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 412);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblSuitability);
+            this.Controls.Add(this.lblNationality);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblPosition);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPosition;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblNationality;
+        private System.Windows.Forms.Label lblSuitability;
+        private System.Windows.Forms.Label lblDescription;
     }
 }
 
